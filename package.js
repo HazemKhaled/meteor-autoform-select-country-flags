59f34f8fa684fde41853419b3ed97bd63eaeb75a
gabriel doroftei <gabriel.doroftei@gmail.com>
2016-01-29 16:14:09 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'newnectar:meteor-autoform-select-country-flags',
-  version: '0.0.1',
+  version: '0.0.3',
   summary: 'Autoform country select flags',
   git: 'https://github.com/NewNectarMedia/meteor-autoform-select-country-flags',
   documentation: 'README.md'
