c87e9846b78a6391270052350d9a5b8e0e8049c6
Hazem Khaled <hazem.khaled@gmail.com>
2018-02-14 17:16:46 +0300

    Tested on autoform 6.x

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'newnectar:meteor-autoform-select-country-flags',
-  version: '0.0.8_2',
+  name: 'newnectar:meteor-autoform-select-country-flags-knawat',
+  version: '1.0.0',
   summary: 'Autoform country select flags',
   git: 'https://github.com/NewNectarMedia/meteor-autoform-select-country-flags',
   documentation: 'README.md'
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0 || 5.0.0');
+  api.use('aldeed:autoform@6.0.0');
   api.use('jss:flag-icon@0.7.3');
   api.use('chhib:selectize-bootstrap-3@0.0.1');
   api.use('jeremy:selectize@0.12.1');
